c9b9e10ae998e3eb4a682f191fe449dec0404020
Felipe Ruhland <felipe.ruhland@gmail.com>
2016-04-17 17:50:37 -0300

    updated settings

--- home/settings.py
+++ home/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'home.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600)
+    'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600)
 }
 
 
